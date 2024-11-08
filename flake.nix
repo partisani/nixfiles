542701541e622b42ac6fe877bf16afc86eff7a2a
Isaac <isaacvgm1@gmail.com>
2024-11-08 18:07:36 -0300

    too many changes

--- flake.nix
+++ flake.nix
@@ -5,12 +5,10 @@
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     };
 
-    outputs = { self, nixpkgs, home-manager, flake-tree, ... }: {
+    outputs = { self, nixpkgs, home-manager, ... }: {
         nixosConfigurations.nix-laptop = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
-            modules = [
-                ./configuration.nix
-            ];
+            modules = [ ./hosts/nix-laptop.nix ];
         };
     };  
 }
